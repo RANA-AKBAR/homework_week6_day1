2e52ebb5277b14333c971fc04c22e0974564cc05
RANA-AKBAR <rana.akbar.08@gmail.com>
2019-07-29 22:51:33 +0100

    -attempt at homework

---
+++ Homework.js
@@ -0,0 +1,11 @@
+
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  console.log(`The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`);
+}
+
+console.log(declareAllSuspects);
